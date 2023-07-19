36adcfa77cb8df61ae0dec4f829ce23aedd1236b
Tinka8 <lenmatka818@gmail.com>
2023-07-19 22:39:00 +0200

    index

---
+++ src/scenes/Game.ts
@@ -0,0 +1,20 @@
+import Phaser from 'phaser'
+
+export default class Game extends Phaser.Scene
+{
+	constructor()
+	{
+		super('game')
+	}
+
+	preload()
+  {
+
+  }
+
+  create()
+  {
+    this.add.image(0, 0, 'tiles')
+  }
+
+}
