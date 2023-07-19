52fb35d16cb9c1c77e87b8c2ab623a1d35583a31
Tinka8 <lenmatka818@gmail.com>
2023-07-20 00:52:37 +0200

    index

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -14,7 +14,20 @@ export default class Game extends Phaser.Scene
 
   create()
   {
-    this.add.image(0, 0, 'tiles')
+    const map = this.make.tilemap({ key: 'dungeon' })
+    const tileset = map.addTilesetImage('dungeon', 'tiles')
+
+    const wallsLayer = map.createLayer('Walls',(tileset as Phaser.Tilemaps.Tileset), 0, 0) as Phaser.Tilemaps.TilemapLayer
+
+    wallsLayer.setCollisionByProperty({ collides: true})
+    
+    const debugGraphics = this.add.graphics().setAlpha(0.7)
+    wallsLayer.renderDebug(debugGraphics, {
+      tileColor: null,
+      collidingTileColor: new Phaser.Display.Color(243, 234, 48, 255),
+      faceColor: new Phaser.Display.Color(40, 39, 37, 255)
+    })
+
   }
 
 }
--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -10,6 +10,7 @@ export default class Preloader extends Phaser.Scene
     preload()
     {
         this.load.image('tiles', 'tiles/dungeon_tiles.png')
+        this.load.tilemapTiledJSON('dungeon', 'tiles/dungeon-01.json')
     }
 
     create() 
