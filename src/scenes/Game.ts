df35fd514e4f24f56785da5e976a09595b75636d
Tinka8 <lenmatka818@gmail.com>
2023-07-21 05:20:23 +0200

    add damage animation

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -6,11 +6,12 @@ import { createCharacterAnims} from '../anims/CharacterAnims'
 
 import Lizard from '../enemies/Lizard'
 
+import '../characters/Faune'
+import Faune from '../characters/Faune'
+
 export default class Game extends Phaser.Scene {
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys
-  private faune!: Phaser.Physics.Arcade.Sprite
-
-  private hit = 0
+  private faune!: Faune
 
 	constructor() {
 		super('game')
@@ -34,10 +35,10 @@ export default class Game extends Phaser.Scene {
 
     // debugDraw(wallsLayer, this)
 
-    this.faune = this.physics.add.sprite(128, 128, 'faune', 'walk-down-3.png')
-    this.faune.body?.setSize(this.faune.width * 0.5, this.faune.height * 0.8)
-
-    this.faune.anims.play('faune-idle-down')
+    this.faune = this.add.faune(128, 128, 'faune')
+    // this.faune = this.physics.add.sprite(128, 128, 'faune', 'walk-down-3.png')
+    // this.faune.body?.setSize(this.faune.width * 0.5, this.faune.height * 0.8)
+    // this.faune.anims.play('faune-idle-down')
 
     this.cameras.main.startFollow(this.faune, true)
 
@@ -65,58 +66,12 @@ export default class Game extends Phaser.Scene {
 
     const dir = new Phaser.Math.Vector2(dx, dy).normalize().scale(200)
 
-    this.faune.setVelocity(dir.x, dir.y)
-
-    this.hit = 1
+    this.faune.handleDamage(dir)
   }
 
   update(t: number, dt: number) {
-    if (this.hit > 0) {
-      ++this.hit
-      if (this.hit > 10) {
-        this.hit = 0
-      }
-    }
-
-
-
-    if (!this.cursors || !this.faune) {
-      return
-    }
-
-    const speed = 100
-    if (this.cursors.left?.isDown) {
-      this.faune.anims.play('faune-run-side', true)
-      this.faune.setVelocity(-speed, 0)
-
-      this.faune.scaleX = - 1
-      this.faune.body.offset.x = 24
-    }
-
-    else if (this.cursors.right?.isDown) {
-      this.faune.anims.play('faune-run-side', true)
-      this.faune.setVelocity(speed, 0)
-
-      this.faune.scaleX = 1
-      this.faune.body.offset.x = 8
-    }
-
-    else if (this.cursors.up?.isDown) {
-      this.faune.anims.play('faune-run-up', true)
-      this.faune.setVelocity(0, -speed)
-    }
-
-    else if (this.cursors.down?.isDown) {
-      this.faune.anims.play('faune-run-down', true)
-      this.faune.setVelocity(0, speed)
-    
-    }
-
-    else {
-      const parts = this.faune.anims.currentAnim.key.split('-')
-      parts[1] = 'idle'
-      this.faune.play(parts.join('-'))
-      this.faune.setVelocity(0, 0)
+    if (this.faune) {
+      this.faune.update(this.cursors)
     }
   }
 }
