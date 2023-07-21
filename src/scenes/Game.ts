51f49bad6bd1dc0e88c62e484ba38ede598f1d7c
Tinka8 <lenmatka818@gmail.com>
2023-07-22 01:25:47 +0200

    ui

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -3,6 +3,7 @@ import Phaser from 'phaser'
 import { debugDraw } from '../utils/debug'
 import { createLizzardAnims } from '../anims/EnemyAnims'
 import { createCharacterAnims } from '../anims/CharacterAnims'
+import { createChestAnims } from '../anims/TreasureAnims'
 
 import Lizard from '../enemies/Lizard'
 
@@ -10,6 +11,7 @@ import '../characters/Faune'
 import Faune from '../characters/Faune'
 
 import { sceneEvents } from '../events/EventsCenter'
+import Chest from '../items/Chest'
 
 
 export default class Game extends Phaser.Scene {
@@ -17,6 +19,7 @@ export default class Game extends Phaser.Scene {
   private faune!: Faune
 
   private knives!: Phaser.Physics.Arcade.Group
+  private lizards!: Phaser.Physics.Arcade.Group
 
   private playerLizardsCollider?: Phaser.Physics.Arcade.Collider
 
@@ -33,6 +36,7 @@ export default class Game extends Phaser.Scene {
 
     createCharacterAnims(this.anims)
     createLizzardAnims(this.anims)
+    createChestAnims(this.anims)
 
     const map = this.make.tilemap({ key: 'dungeon' })
     const tileset = map.addTilesetImage('dungeon', 'tiles', 16, 16, 1, 2)
@@ -41,51 +45,86 @@ export default class Game extends Phaser.Scene {
     const groundLayer = map.createLayer("Ground", tileset as Phaser.Tilemaps.Tileset, 0, 0) as Phaser.Tilemaps.TilemapLayer;
 
     wallsLayer.setCollisionByProperty({ collides: true })
+    
+    this.knives = this.physics.add.group({
+			classType: Phaser.Physics.Arcade.Image,
+			maxSize: 3
+		})
+
+    const chests = this.physics.add.staticGroup({
+			classType: Chest
+		})
+
+		const chestsLayer = map.getObjectLayer('Chests')
+
+		chestsLayer.objects.forEach(chestObj => {
+			chests.get(chestObj.x! + chestObj.width! * 0.5, chestObj.y! - chestObj.height! * 0.5, 'treasure')
+		})
 
     this.faune = this.add.faune(128, 128, 'faune')
     this.faune.setKnives(this.knives)
   
-    this.cameras.main.startFollow(this.faune, true)
+   	this.cameras.main.startFollow(this.faune, true)
 
-  const lizards = this.physics.add.group({
-      classType: Lizard,
-      createCallback: (go) => {
-        const lizGo = go as Lizard
-        lizGo.body.onCollide = true
-      }
-    })
+		this.lizards = this.physics.add.group({
+			classType: Lizard,
+			createCallback: (go) => {
+				const lizGo = go as Lizard
+				lizGo.body.onCollide = true
+			}
+		})
 
-    lizards.get(256, 128, 'lizard')
+		const lizardsLayer = map.getObjectLayer('Lizards')
+		lizardsLayer.objects.forEach(lizObj => {
+			this.lizards.get(lizObj.x! + lizObj.width! * 0.5, lizObj.y! - lizObj.height! * 0.5, 'lizard')
+		})
 
-    this.physics.add.collider(this.faune, wallsLayer)
-    this.physics.add.collider(lizards, wallsLayer)
+		this.physics.add.collider(this.faune, wallsLayer)
+		this.physics.add.collider(this.lizards, wallsLayer)
 
-    this.physics.add.collider(lizards, this.faune, this.handlePlayerLizzardCollision, undefined, this)
-  }
+		this.physics.add.collider(this.faune, chests, this.handlePlayerChestCollision, undefined, this)
 
+		this.physics.add.collider(this.knives, wallsLayer, this.handleKnifeWallCollision, undefined, this)
+		this.physics.add.collider(this.knives, this.lizards, this.handleKnifeLizardCollision, undefined, this)
 
-  private handleKnifeWallCollision(obj1: Phaser.GameObjects.GameObject, obj2: Phaser.GameObjects.GameObject) {
+		this.playerLizardsCollider = this.physics.add.collider(this.lizards, this.faune, this.handlePlayerLizardCollision, undefined, this)
+	}
+
+	private handlePlayerChestCollision(obj1: Phaser.GameObjects.GameObject, obj2: Phaser.GameObjects.GameObject) {
+		const chest = obj2 as Chest
+		this.faune.setChest(chest)
+	}
+
+	private handleKnifeWallCollision(obj1: Phaser.GameObjects.GameObject, obj2: Phaser.GameObjects.GameObject) {
 		this.knives.killAndHide(obj1)
 	}
 
+	private handleKnifeLizardCollision(obj1: Phaser.GameObjects.GameObject, obj2: Phaser.GameObjects.GameObject) {
+		this.knives.killAndHide(obj1)
+		this.lizards.killAndHide(obj2)
+	}
 
+	private handlePlayerLizardCollision(obj1: Phaser.GameObjects.GameObject, obj2: Phaser.GameObjects.GameObject) {
+		const lizard = obj2 as Lizard
+		
+		const dx = this.faune.x - lizard.x
+		const dy = this.faune.y - lizard.y
 
-  private handlePlayerLizzardCollision(obj1: Phaser.GameObjects.GameObject, obj2: Phaser.GameObjects.GameObject) {
-    const lizard = obj2 as Lizard
+		const dir = new Phaser.Math.Vector2(dx, dy).normalize().scale(200)
 
-    const dx = this.faune.x - lizard.x
-    const dy = this.faune.y - lizard.y
+		this.faune.handleDamage(dir)
 
-    const dir = new Phaser.Math.Vector2(dx, dy).normalize().scale(200)
+		sceneEvents.emit('player-health-changed', this.faune.health)
 
-    this.faune.handleDamage(dir)
+		if (this.faune.health <= 0) {
+			this.playerLizardsCollider?.destroy()
+		}
+	}
+	
+	update(t: number, dt: number) {
 
-    sceneEvents.emit('player-health-changed', this.faune.health)
-  }
-
-  update(t: number, dt: number) {
-    if (this.faune) {
-      this.faune.update(this.cursors)
-    }
-  }
+		if (this.faune) {
+			this.faune.update(this.cursors)
+		}
+	}
 }
--- src/scenes/GameUI.ts
+++ src/scenes/GameUI.ts
@@ -9,33 +9,43 @@ export default class GameUI extends Phaser.Scene {
         super({ key: 'game-ui' })
     }
 
-    create() {
-        this.hearts = this.add.group({
-            classType: Phaser.GameObjects.Image
-        })
+	create() {
+		this.add.image(6, 26, 'treasure', 'coin_anim_f0.png')
+		const coinsLabel = this.add.text(12, 20, '0', {
+			fontSize: '14'
+		})
 
-        this.hearts.createMultiple({
-            key: 'ui-heart-full',
-            setXY: {
-                x: 10,
-                y: 10,
-                stepX: 16
-            },
-            quantity:  3
-        })
+		sceneEvents.on('player-coins-changed', (coins: number) => {
+			coinsLabel.text = coins.toLocaleString()
+		})
 
-        sceneEvents.on('player-health-changed', this.handlePlayerHealthChanged, this)
+		this.hearts = this.add.group({
+			classType: Phaser.GameObjects.Image
+		})
 
-        this.events.once(Phaser.Scenes.Events.SHUTDOWN, () => {
-            sceneEvents.off('player-health-changed', this.handlePlayerHealthChanged, this)    
-        })
-    }
+		this.hearts.createMultiple({
+			key: 'ui-heart-full',
+			setXY: {
+				x: 10,
+				y: 10,
+				stepX: 16
+			},
+			quantity: 3
+		})
 
-    private handlePlayerHealthChanged(health: number) {
+		sceneEvents.on('player-health-changed', this.handlePlayerHealthChanged, this)
+
+		this.events.once(Phaser.Scenes.Events.SHUTDOWN, () => {
+			sceneEvents.off('player-health-changed', this.handlePlayerHealthChanged, this)
+			sceneEvents.off('player-coins-changed')
+		})
+	}
+
+	private handlePlayerHealthChanged(health: number) {
 		this.hearts.children.each((go, idx) => {
 			const heart = go as Phaser.GameObjects.Image
-
-			if (idx < health) {
+			
+            if (idx < health) {
 				heart.setTexture('ui-heart-full')
 			}
 
